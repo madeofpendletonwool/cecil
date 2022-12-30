d7677afcc9b97f4e18ae9e15a80e634d21983f39
brickinthewall10 <blacanwite@gmail.com>
2022-12-30 14:12:03 -0600

    $ sign

--- createcronvars.sh
+++ createcronvars.sh
@@ -5,4 +5,4 @@ set -f
 set -o noglob
 export DOCKERCRON=$(echo $DOCKER_MONITOR_CRON | xargs)
 export LINUXCRON=$(echo $LINUX_HEALTH_CRON | xargs)
-export DYNAMICCRON=&(echo $DYNAMIC_IP_CRON | xargs)
+export DYNAMICCRON=$(echo $DYNAMIC_IP_CRON | xargs)
