b528221d551beeb809a1258cece6dd0a10cb4880
Collin Pendleton <collinp@collinpendleton.com>
2023-01-17 16:09:28 -0600

    Adding Functionality

--- webapp.py
+++ webapp.py
@@ -23,6 +23,21 @@ def main(page: Page):
         time.sleep(.3)
         page.update()
 
+    def open_mail_settings(e):
+        page.go("/settings/mail")
+
+    def open_settings(e):
+        page.go("/settings")
+
+    def open_idrac(e):
+        page.go("/idrac")
+    def open_dockermon(e):
+        page.go("/dockermon")
+    def open_linuxhealth(e):
+        page.go("/linuxhealth")
+    def open_dynamicip(e):
+        page.go("/dynamicip")
+
     page.title = "Cecil"
     page.theme_mode = "light"
 
@@ -34,11 +49,23 @@ def main(page: Page):
     page.appbar = AppBar(title=Text("Cecil - Alerting and Monitoring", color="white"), center_title=True, bgcolor="blue",
                         actions=[theme_icon_button], )
 
-    def open_mail_settings(e):
-        page.go("/settings/mail")
 
-    def open_settings(e):
-        page.go("/settings")
+    cecil_info = """
+    Welcome to Cecil! This application is an alert and monitoring app built to be as generic as possible with 'modules' built-in to provide functionality. 
+    Modules can be used, or can also be ignored simply by selecting them and setting them up. Feel free to click around and utilize them to your heart's content. 
+    They all simply require a very easy setup and the app will walk you through it!
+
+    """
+
+    cecil_text = ft.Text(cecil_info, style=ft.TextThemeStyle.BODY_MEDIUM, text_align=ft.TextAlign.CENTER, size=16)
+    cecil_row = Row(alignment=ft.MainAxisAlignment.CENTER, controls=[cecil_text])
+
+    dell_button = ElevatedButton("iDrac Server Health Report", on_click=open_idrac)
+    docker_monitor_button = ElevatedButton("Docker Monitor", on_click=open_dockermon)
+    linux_health_button = ElevatedButton("Linux Health Report", on_click=open_linuxhealth)
+    Dynamic_ip_button = ElevatedButton("Dynamic IP Checker", on_click=open_dynamicip)
+
+    modules_row = ft.Row(alignment=ft.MainAxisAlignment.CENTER, controls=[dell_button, docker_monitor_button, linux_health_button, Dynamic_ip_button])
 
 
 
@@ -51,32 +78,55 @@ def main(page: Page):
                 [
                     AppBar(title=Text("Cecil - Alerting and Monitoring", color="white"), center_title=True, bgcolor="blue",
                         actions=[theme_icon_button], ),
-                    ElevatedButton("Go to settings", on_click=open_settings),
+                    cecil_row,
+                    modules_row
                 ],
             )
         )
-        if page.route == "/settings" or page.route == "/settings/mail":
+        if page.route == "/dynamicip" or page.route == "/dynamicip":
             page.views.append(
                 View(
-                    "/settings",
+                    "/dynamicip",
                     [
-                        AppBar(title=Text("Settings"), bgcolor=colors.SURFACE_VARIANT),
-                        Text("Settings!", style="bodyMedium"),
-                        ElevatedButton(
-                            "Go to mail settings", on_click=open_mail_settings
-                        ),
+                        AppBar(title=Text("Cecil - Alerting and Monitoring", color="white"), center_title=True, bgcolor="blue",
+                        actions=[theme_icon_button], ),
+                    Text('DynamicIP Scanner Setup page!')
                     ],
                 )
             )
-        if page.route == "/settings/mail":
+        if page.route == "/linuxhealth" or page.route == "/linuxhealth":
             page.views.append(
                 View(
-                    "/settings/mail",
+                    "/linuxhealth",
                     [
-                        AppBar(
-                            title=Text("Mail Settings"), bgcolor=colors.SURFACE_VARIANT
-                        ),
-                        Text("Mail settings!"),
+                        AppBar(title=Text("Cecil - Alerting and Monitoring", color="white"), center_title=True, bgcolor="blue",
+                        actions=[theme_icon_button], ),
+                    Text('Linux Health Scan Setup page!')
+                    ],
+                )
+            )
+        if page.route == "/dockermon":
+            page.views.append(
+                View(
+                    "/dockermon",
+                    [
+                        AppBar(title=Text("Cecil - Alerting and Monitoring", color="white"), center_title=True, bgcolor="blue",
+                        actions=[theme_icon_button], ),
+                    Text('Docker Monitor Setup page!')
+                    ],
+                )
+            )
+        if page.route == "/idrac":
+            page.views.append(
+                View(
+                    "/idrac",
+                    [
+                        AppBar(title=Text("Cecil - Alerting and Monitoring", color="white"), center_title=True, bgcolor="blue",
+                        actions=[theme_icon_button], ),
+                    Text('The iDrac Report setup page. '),
+                    ft.TextField(label="IP Address", hint_text="ex. 10.0.0.1"),
+                    ft.TextField(label="Username", hint_text="ex. root"),
+                    ft.TextField(label="Password", can_reveal_password=True, password=True, hint_text="ex. password1")
                     ],
                 )
             )
@@ -91,63 +141,9 @@ def main(page: Page):
     page.on_route_change = route_change
     page.on_view_pop = view_pop
 
-    test = Text("Hello")
-    page.add(test, ElevatedButton("Go to settings", on_click=open_settings))
+    page.add(cecil_row, modules_row)
 
-    # page.go(page.route)
-
-
-
-
-    # def route_change(e):
-    #     print("Route change:", e.route)
-    #     page.views.clear()
-    #     page.views.append(
-    #         View(
-    #             "/",
-    #             [
-    #                 AppBar(title=Text("Cecil - Alerts, Monitoring, and Home for Information"), center_title=True),
-    #                 ElevatedButton("Go to settings", on_click=open_settings),
-    #             ],
-    #         )
-    #     )
-    #     if page.route == "/settings" or page.route == "/settings/mail":
-    #         page.views.append(
-    #             View(
-    #                 "/settings",
-    #                 [
-    #                     AppBar(title=Text("Settings"), bgcolor=colors.SURFACE_VARIANT),
-    #                     Text("Settings!", style="bodyMedium"),
-    #                     ElevatedButton(
-    #                         "Go to mail settings", on_click=open_mail_settings
-    #                     ),
-    #                 ],
-    #             )
-    #         )
-    #     if page.route == "/settings/mail":
-    #         page.views.append(
-    #             View(
-    #                 "/settings/mail",
-    #                 [
-    #                     AppBar(
-    #                         title=Text("Mail Settings"), bgcolor=colors.SURFACE_VARIANT
-    #                     ),
-    #                     Text("Mail settings!"),
-    #                 ],
-    #             )
-    #         )
-    #     page.update()
-
-    # def view_pop(e):
-    #     print("View pop:", e.view)
-    #     page.views.pop()
-    #     top_view = page.views[-1]
-    #     page.go(top_view.route)
-
-    # page.on_route_change = route_change
-    # page.on_view_pop = view_pop
-
-    # page.go(page.route)
-
-
+# Browser Version
 ft.app(target=main, view=ft.WEB_BROWSER)
+# App Version
+# ft.app(target=main, port=8034)
